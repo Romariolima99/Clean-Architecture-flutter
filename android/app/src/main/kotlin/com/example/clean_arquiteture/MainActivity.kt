161c14330457c5f2bbacdc5935f75083dd6fadfe
Romário Lima <devromariolima@gmail.com>
2024-09-30 13:58:23 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/clean_arquiteture/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.clean_arquiteture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/layers/domain/entitys/carro_entity.dart
@@ -0,0 +1,16 @@
+class Carro {
+
+String placa;
+int qPortas;
+double valor;
+
+
+Carro({
+required this.placa, 
+required this.qPortas, 
+required this.valor
+});
+
+
+
+}
