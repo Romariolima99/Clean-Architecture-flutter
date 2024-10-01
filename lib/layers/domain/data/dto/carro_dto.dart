ebd13c2af56d011d8f1fbc09fb358f0ab15162d3
Romário Lima <devromariolima@gmail.com>
2024-10-01 16:59:33 -0400

    CLEAN ARCHITECTURE: DTO PATTERN e DATA LAYER

---
+++ lib/layers/domain/data/dto/carro_dto.dart
@@ -0,0 +1,29 @@
+import 'package:clean_arquiteture/layers/domain/entitys/carro_entity.dart';
+
+class CarroDto extends CarroEntity {
+  String placa;
+  int quantidadePortas;
+  double valorFinal;
+
+  CarroDto({
+    required this.placa,
+    required this.quantidadePortas,
+    required this.valorFinal,
+  }) : super(placa: placa, qPortas: quantidadePortas, valor: valorFinal);
+
+  Map toMap() {
+    return {
+      'placa': this.placa,
+      'quantidadePortas': this.quantidadePortas,
+      'valorFinal': this.valorFinal,
+    };
+  }
+
+  CarroDto fronMap(Map map) {
+    return CarroDto(
+      placa: map['placa'],
+      quantidadePortas: map['quantidadePortas'],
+      valorFinal: map['valorFinal'],
+    );
+  }
+}
